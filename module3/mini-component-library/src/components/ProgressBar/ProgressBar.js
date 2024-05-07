49d8c4b89257fb3c1d8b6bc4c50b4b3342dba48f
Peter Tolsma <pejato@umich.edu>
2024-05-07 15:19:25 -0400

    My try at progress bar

--- module3/mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ module3/mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,66 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--wrapper-radius": "4px",
+    "--bar-radius": "4px",
+    "--padding": "0px",
+  },
+  medium: {
+    "--height": "12px",
+    "--wrapper-radius": "4px",
+    "--bar-radius": "4px",
+    "--padding": "0px",
+  },
+  large: {
+    "--height": "16px",
+    "--wrapper-radius": "8px",
+    "--bar-radius": "4px",
+    "--padding": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const style = {
+    ...SIZES[size],
+    "--width": `${Math.min(100, Math.max(0, value))}%`,
+  };
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-valuenow={value}
+      style={style}
+    >
+      <Bar style={style} />
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border: 0px solid transparent;
+  border-radius: var(--wrapper-radius);
+  box-shadow: inset 0 2px 4px 0 ${COLORS.transparentGray35};
+  padding: var(--padding);
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  height: var(--height);
+  width: 100%;
+  border: 0px solid transparent;
+  clip-path: rect(0 0 100% var(--width));
+  border-radius: var(--bar-radius);
+  display: block;
+`;
 
 export default ProgressBar;
