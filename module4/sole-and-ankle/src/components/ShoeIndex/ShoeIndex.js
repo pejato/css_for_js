086fb4290471854181e5250093d6c3e08e5ef8f5
Peter Tolsma <pejato@umich.edu>
2024-05-10 21:31:49 -0400

    Exercise 3: Had to modify layout of Select component, not sure how I feel about it

--- module4/sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
+++ module4/sole-and-ankle/src/components/ShoeIndex/ShoeIndex.js
@@ -25,16 +25,18 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <BreadcrumbsWrapper>
+          <Breadcrumbs>
+            <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+            <Breadcrumbs.Crumb href="/sale/shoes">
+              Shoes
+            </Breadcrumbs.Crumb>
+          </Breadcrumbs>
+        </BreadcrumbsWrapper>
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -42,13 +44,29 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 32px;
+  flex-direction: row-reverse;
+  align-items: baseline;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex: 1;
+`;
 
-const MainColumn = styled.div``;
+const BreadcrumbsWrapper = styled.div`
+  white-space: nowrap;
+`;
 
-const Header = styled.header``;
+const MainColumn = styled.div`
+  flex: 4;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
