3bd3155e048df53c336f8d38372d0789feb6b320
Peter Tolsma <pejato@umich.edu>
2024-05-16 13:03:22 -0500

    Remove filter on phone, move breadcrumbs to above header on mobile, remove left category sidebar on mobile

--- module5/sole-and-ankle-revisited/src/components/ShoeIndex/ShoeIndex.js
+++ module5/sole-and-ankle-revisited/src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,32 +9,45 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
+const ShoeIndexBreadCrumbs = () => {
+  return (
+    <BreadCrumbsWrapper>
+      <Breadcrumbs>
+        <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+        <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+        <Breadcrumbs.Crumb href="/sale/shoes">
+          Shoes
+        </Breadcrumbs.Crumb>
+      </Breadcrumbs>
+    </BreadCrumbsWrapper>
+  )
+}
+
+const BreadCrumbsWrapper = styled.div``
+
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
+        <ShoeIndexBreadCrumbs />
         <Header>
           <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <FilterWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </FilterWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <ShoeIndexBreadCrumbs />
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -51,16 +64,33 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
   flex: 1;
+  & > ${BreadCrumbsWrapper} {
+    display: none;
+  }
+  @media ${QUERIES.tabletAndDown} {
+    & > ${BreadCrumbsWrapper} {
+      display: initial;
+    }
+  }
 `;
 
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+`;
+
+const FilterWrapper = styled.div`
+  @media ${QUERIES.phoneAndDown} {
+    display: none;
+  }
 `;
 
 const Title = styled.h2`
